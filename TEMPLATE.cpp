6d4cd0923e0de543d2b08997b26065cb8e35c3e8
Nafisul Islam Nazmi <nazmi.ndc@gmail.com>
2016-08-06 23:41:47 +0600

    Add files via upload

---
+++ TEMPLATE.cpp
@@ -0,0 +1,56 @@
+//Nafisul Islam Nazmi
+//Problem:
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <map>
+#include <vector>
+#include <set>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+typedef long long i64;
+typedef unsigned long long ui64;
+typedef vector<int> vint;
+typedef vector<long long> vi64;
+typedef vector<string> vstr;
+typedef vector<bool> vbool;
+typedef map<string, int> mstr;
+typedef map<int, int> mint;
+
+#define nl '\n'
+#define eps 1e-9
+#define inf 10e9
+#define neginf -10e9
+#define pb push_back
+#define popb pop_back
+#define forf(i, a, b) for(i = a; i < b; i++)
+#define forb(i, a, b) for(i = a; i > b; i--)
+#define forfi(i, a, b) for(i = a; i <= b; i++)
+#define forbi(i, a, b) for(i = a; i >= b; i--)
+#define forit(t, it) for(typeof(t.begin() it = t.begin(); it != t.end(); it++)
+
+template<class T>
+inline T sqr(T x)
+{
+    return x * x;
+}
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+
+    return 0;
+}
+
