47850542e7b8a640b84aa2f8a80275e501536891
Nafisul Islam Nazmi <nazmi.ndc@gmail.com>
2016-08-11 23:38:03 +0600

    Add files via upload

--- TEMPLATE.cpp
+++ TEMPLATE.cpp
@@ -1,34 +1,20 @@
 //Nafisul Islam Nazmi
 //Problem:
 
-#include <iostream>
-#include <stdio.h>
-#include <math.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <map>
-#include <vector>
-#include <set>
-#include <string>
-#include <iomanip>
-
-using namespace std;
-
 typedef long long i64;
 typedef unsigned long long ui64;
 typedef vector<int> vint;
 typedef vector<long long> vi64;
 typedef vector<string> vstr;
+typedef vector<char> vchar;
+typedef string str;
 typedef vector<bool> vbool;
 typedef map<string, int> mstr;
 typedef map<int, int> mint;
 typedef map<string, int>::iterator mstrit;
 typedef map<int, int>::iterator mintit;
 
+#define endl '\n'
 #define nl '\n'
 #define eps 1e-9
 #define inf 10e9
@@ -40,10 +26,12 @@ typedef map<int, int>::iterator mintit;
 #define forfi(i, a, b) for(i = a; i <= b; i++)
 #define forbi(i, a, b) for(i = a; i >= b; i--)
 #define forit(t, it) for(typeof(t.begin() it = t.begin(); it != t.end(); it++)
-
+int tc = 0;
 template<class T>inline T sqr(T x){return x * x;}
+i64 factorial(i64 n){i64 i, f = 1; if(i <= 1)return 1; forfi(i, 1, n) f*=i; return f;}
+void printtc(){cout << "Case " << ++tc;}
 
-void do()
+void solution()
 {
 
 }
@@ -51,7 +39,7 @@ void do()
 int main()
 {
     ios::sync_with_stdio(false);
-    //do();
+    //solution();
 
 
     return 0;
