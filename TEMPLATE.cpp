5224948540a3823757301c7f873788e7226dffc5
Nafisul Islam Nazmi <nazmi.ndc@gmail.com>
2016-08-12 15:06:46 +0600

    Add files via upload

--- TEMPLATE.cpp
+++ TEMPLATE.cpp
@@ -1,6 +1,23 @@
 //Nafisul Islam Nazmi
 //Problem:
 
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <map>
+#include <vector>
+#include <set>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
 typedef long long i64;
 typedef unsigned long long ui64;
 typedef vector<int> vint;
